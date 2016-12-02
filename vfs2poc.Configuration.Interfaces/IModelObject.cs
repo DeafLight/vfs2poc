092bbcaa47f47052079ea713383f19d7973e88ba
dev-i-ant <xavlec@gmail.com>
2016-12-02 22:13:37 +0100

    ModelObject

---
+++ vfs2poc.Configuration.Interfaces/IModelObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vfs2poc.Configuration.Interfaces
+{
+    /// <summary>
+    /// Global main class.
+    /// Ensures that every object has an Id and can be set to fixed if part of a functional module.
+    /// </summary>
+    public interface IModelObject
+    {
+        /// <summary>
+        /// The object Id
+        /// </summary>
+        Guid? Id { get; set; }
+
+        /// <summary>
+        /// If true, this object is part of a functional module and cannot be deleted
+        /// </summary>
+        bool? IsFixed { get; set; }
+    }
+}
---
+++ vfs2poc.Configuration.Model/ModelObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using vfs2poc.Configuration.Interfaces;
+
+namespace vfs2poc.Configuration.Model
+{
+    public class ModelObject : IModelObject
+    {
+        public Guid? Id { get; set; }
+
+        public bool? IsFixed { get; set; }
+    }
+}
